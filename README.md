85503b407515ef099a65babc3b6b81151ccb34e2
Satrio <satriowaysscoot@gmail.com>
2024-02-25 14:08:29 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# variants
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/variants/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.variants
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
